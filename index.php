bf6cfe8a542ad713930748c146461b73d3430ffc
Alexa Ares <Alexa@Alexas-MacBook-Pro.local>
2017-03-16 19:09:42 +0100

    iniciar

--- index.php
+++ index.php
@@ -3,6 +3,25 @@
         <meta charset="utf-8"
     </head>
         <body>
+                <?php
+
+$nombre="Albert";
+if (isset($_POST['nombre'])){
+    $nombre=$_POST['nombre']));
+}
+
+echo "Hola".$nombre;
+
+?>
+        
+        <form action="index.php" method="post">
+            <input type="text" name="nombre" placeholder="escribe tu nombre">
+            <br>
+            <input type="submit" value="enviar"
+        
+        </form>
         
         </body>
+
+        
 </html>
